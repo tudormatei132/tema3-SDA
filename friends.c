41e537a1ceab8689adbc9e1315eecf871dc402ee
Sebastopol27 <112060980+Sebastopol27@users.noreply.github.com>
2024-05-22 01:45:43 +0300

    Add files via upload

--- friends.c
+++ friends.c
@@ -8,24 +8,6 @@
 
 #define INF 999
 
-typedef struct ll_node_t
-{
-	void  *data;
-	struct ll_node_t *next;
-} ll_node_t;
-
-typedef struct linked_list_t
-{
-	ll_node_t *head;
-	unsigned int data_size;
-	unsigned int size;
-} linked_list_t;
-
-typedef struct stack_t
-{
-	struct linked_list_t *list;
-} stack;
-
 typedef struct queue_t
 {
 	/* Dimensiunea maxima a cozii */
@@ -42,7 +24,10 @@ typedef struct queue_t
 	void **buff;
 } queue_t;
 
-matrix_graph_t *mg_create(int nodes)
+//---------graph_functions------------
+
+matrix_graph_t *
+mg_create(int nodes)
 {
 	matrix_graph_t *mg = malloc(sizeof(*mg));
 
@@ -54,14 +39,12 @@ matrix_graph_t *mg_create(int nodes)
 	return mg;
 }
 
-/* Adauga o muchie intre nodurile sursa si destinatie */
 void
 mg_add_edge(matrix_graph_t *graph, int src, int dest)
 {
 	graph->matrix[src][dest] = 1;
 }
 
-/* Returneaza 1 daca exista muchie intre cele doua noduri, 0 in caz contrar */
 int
 mg_has_edge(matrix_graph_t *graph, int src, int dest)
 {
@@ -70,14 +53,12 @@ mg_has_edge(matrix_graph_t *graph, int src, int dest)
 	return 0;
 }
 
-/* Elimina muchia dintre nodurile sursa si destinatie */
 void
 mg_remove_edge(matrix_graph_t *graph, int src, int dest)
 {
 	graph->matrix[src][dest] = 0;
 }
 
-/* Elibereaza memoria folosita de matricea de adiacenta a grafului */
 void
 mg_free(matrix_graph_t *graph)
 {
@@ -88,221 +69,7 @@ mg_free(matrix_graph_t *graph)
 	free(graph);
 }
 
-void
-print_matrix_graph(matrix_graph_t *mg)
-{
-	for (int i = 0; i < mg->nodes; i++) {
-		for (int j = 0; j < mg->nodes; j++)
-			printf("%d ", mg->matrix[i][j]);
-		printf("\n");
-	}
-}
-
-linked_list_t *
-ll_create(unsigned int data_size)
-{
-	linked_list_t *ll;
-
-	ll = malloc(sizeof(*ll));
-
-	ll->head = NULL;
-	ll->data_size = data_size;
-	ll->size = 0;
-
-	return ll;
-}
-
-ll_node_t *get_nth_node(linked_list_t *list, unsigned int n)
-{
-	if (!list)
-		return NULL;
-
-	unsigned int len = list->size - 1;
-	unsigned int i;
-	ll_node_t *node = list->head;
-
-	if (n < len)
-		n = n;
-	else
-		n = len;
-
-	for (i = 0; i < n; ++i)
-		node = node->next;
-
-	return node;
-}
-
-void
-ll_add_nth_node(linked_list_t *list, unsigned int n, const void *new_data)
-{
-	ll_node_t *prev, *curr;
-	ll_node_t *new_node;
-
-	if (!list)
-		return;
-
-	/* n >= list->size inseamna adaugarea unui nou nod la finalul listei. */
-	if (n > list->size)
-		n = list->size;
-
-	curr = list->head;
-	prev = NULL;
-	while (n > 0) {
-		prev = curr;
-		curr = curr->next;
-		--n;
-	}
-
-	new_node = malloc(sizeof(*new_node));
-	new_node->data = malloc(list->data_size);
-	memcpy(new_node->data, new_data, list->data_size);
-
-	new_node->next = curr;
-	if (!prev) {
-		/* Adica n == 0. */
-		list->head = new_node;
-	} else {
-		prev->next = new_node;
-	}
-
-	list->size++;
-}
-
-ll_node_t *
-ll_remove_nth_node(linked_list_t *list, unsigned int n)
-{
-	ll_node_t *prev, *curr;
-
-	if (!list || !list->head)
-		return NULL;
-
-	/* n >= list->size - 1 inseamna eliminarea nodului de la finalul listei. */
-	if (n > list->size - 1)
-		n = list->size - 1;
-
-	curr = list->head;
-	prev = NULL;
-	while (n > 0) {
-		prev = curr;
-		curr = curr->next;
-		--n;
-	}
-
-	if (!prev) {
-		/* Adica n == 0. */
-		list->head = curr->next;
-	} else {
-		prev->next = curr->next;
-	}
-
-	list->size--;
-
-	return curr;
-}
-
-unsigned int
-ll_get_size(linked_list_t *list)
-{
-	if (!list)
-		return -1;
-
-	return list->size;
-}
-
-void
-ll_free(linked_list_t **pp_list)
-{
-	ll_node_t *curr;
-
-	if (!pp_list || !*pp_list)
-		return;
-
-	while (ll_get_size(*pp_list) > 0) {
-		curr = ll_remove_nth_node(*pp_list, 0);
-		free(curr->data);
-		curr->data = NULL;
-		free(curr);
-		curr = NULL;
-	}
-
-	free(*pp_list);
-	*pp_list = NULL;
-}
-
-//---------stack functions
-
-stack *
-st_create(unsigned int data_size)
-{
-	stack *st = malloc(sizeof(*st));
-	st->list = ll_create(data_size);
-
-	return st;
-}
-
-unsigned int
-st_get_size(stack *st)
-{
-	if (!st || !st->list)
-		return 0;
-	return st->list->size;
-}
-
-unsigned int
-st_is_empty(stack *st)
-{
-	return !st || !st->list || !st->list->size;
-}
-
-void *
-st_peek(stack *st)
-{
-	if (!st || !st->list || !st->list->size)
-		return NULL;
-
-	return st->list->head->data;
-}
-
-void
-st_pop(stack *st)
-{
-	if (!st || !st->list)
-		return;
-
-	ll_node_t *rem = ll_remove_nth_node(st->list, 0);
-	free(rem->data);
-	free(rem);
-}
-
-void
-st_push(stack *st, void *new_data)
-{
-	if (!st || !st->list)
-		return;
-
-	ll_add_nth_node(st->list, 0, new_data);
-}
-
-void
-st_clear(stack *st)
-{
-	if (!st || !st->list)
-		return;
-
-	ll_free(&st->list);
-}
-
-void
-st_free(stack *st)
-{
-	if (!st || !st->list)
-		return;
-
-	ll_free(&st->list);
-	free(st);
-}
-
-//---------queue functions
+//---------queue_functions-----------
 
 queue_t *
 q_create(unsigned int data_size, unsigned int max_size)
@@ -397,13 +164,14 @@ void
 dfs(matrix_graph_t *mg, int node1, int *distance)
 {
 	int *color = malloc(sizeof(int) * mg->nodes);
+	//the initialization of arrays
 	for (int i = 0; i < mg->nodes; i++) {
-		color[i] = 0;
+		color[i] = 0; //node unvisited
 		distance[i] = INF;
 	}
 
 	queue_t *Q = q_create(sizeof(int), mg->nodes);
-	color[node1] = 1;
+	color[node1] = 1; //is being vesited
 	distance[node1] = 0;
 
 	q_enqueue(Q, &node1);
@@ -416,125 +184,138 @@ dfs(matrix_graph_t *mg, int node1, int *distance)
 			if (mg_has_edge(mg, v, i)) {
 				if (color[i] == 0) {
 					color[i] = 1;
+					//calculate the distance
 					distance[i] = distance[v] + 1;
 					q_enqueue(Q, &i);
 				}
 			}
 		}
-		color[v] = 2;
+		color[v] = 2; //it's visitation has finished
 	}
-	q_free(Q);
+	q_free(Q); //clear memory
 	free(color);
 }
 
+//------FUNCTIONS---------
+
 void add_friend(char *cmd, matrix_graph_t *mg)
 {
-	cmd = strtok(NULL, "\n ");
+	cmd = strtok(NULL, "\n "); //get the names
 	char *name1 = strdup(cmd);
 	cmd = strtok(NULL, "\n ");
 	char *name2 = strdup(cmd);
 
-	int id1 = get_user_id(name1);
+	int id1 = get_user_id(name1); //get the id of each name
 	int id2 = get_user_id(name2);
 
-	mg_add_edge(mg, id1, id2);
-	mg_add_edge(mg, id2, id1);
+	mg_add_edge(mg, id1, id2); //create the link between names
+	mg_add_edge(mg, id2, id1); //in both directions
 
 	if (mg->matrix[id2][id1] == 1 && mg->matrix[id1][id2] == 1)
 		printf("Added connection %s - %s\n", name1, name2);
 
-	free(name1);
+	free(name1); //clear the memory
 	free(name2);
 }
 
 void remove_friend(char *cmd, matrix_graph_t *mg)
 {
-	cmd = strtok(NULL, "\n ");
+	cmd = strtok(NULL, "\n "); //get the names
 	char *name1 = strdup(cmd);
 	cmd = strtok(NULL, "\n ");
 	char *name2 = strdup(cmd);
 
-	int id1 = get_user_id(name1);
+	int id1 = get_user_id(name1); //get the id of each name
 	int id2 = get_user_id(name2);
 
-	mg_remove_edge(mg, id1, id2);
-	mg_remove_edge(mg, id2, id1);
+	mg_remove_edge(mg, id1, id2); //remove the link between names
+	mg_remove_edge(mg, id2, id1); //in both directions
 
 	if (mg->matrix[id2][id1] == 0 && mg->matrix[id1][id2] == 0)
 		printf("Removed connection %s - %s\n", name1, name2);
 
-	free(name1);
+	free(name1); //clear the memory
 	free(name2);
 }
 
 void suggestions(char *cmd, matrix_graph_t *mg)
 {
-	cmd = strtok(NULL, "\n ");
+	cmd = strtok(NULL, "\n "); //get the name
 	char *name1 = strdup(cmd);
 
-	int id1 = get_user_id(name1);
+	int id1 = get_user_id(name1); //get the id of the name
 
+	//the array in which we save 1 for the possible friends
 	int *possible_friends = calloc(mg->nodes, sizeof(int));
-	int ok = 1;
 
+	int ok = 1; //verifies that there is a possible friend
 	for (int i = 0; i < mg->nodes; i++) {
-		if (mg->matrix[id1][i])
+		if (mg->matrix[id1][i]) //for every friend
 			for (int j = 0; j < mg->nodes; j++)
-				if (mg->matrix[i][j] == 1) {
-					possible_friends[j] = 1;
-					ok = 0;
+				if (mg->matrix[i][j] == 1) { //go through all friends' friends
+					possible_friends[j] = 1; //update the variables
+					ok = 0; //there is at least one friend
 				}
 	}
 
 	if (ok == 0) {
 		printf("Suggestions for %s:\n", name1);
+		//go through the array of possible friends
 		for (int i = 0; i < mg->nodes; i++)
+			//if the value is 1 and it is not friend with out initial name
 			if (possible_friends[i] == 1 && i != id1 && mg->matrix[id1][i] == 0)
 				printf("%s\n", get_user_name(i));
 	} else {
 		printf("There are no suggestions for %s\n", name1);
 	}
 
-	free(possible_friends);
+	free(possible_friends); //clear the memory
 	free(name1);
 }
 
 void distance(char *cmd, matrix_graph_t *mg)
 {
-	cmd = strtok(NULL, "\n ");
+	cmd = strtok(NULL, "\n "); //get the names
 	char *name1 = strdup(cmd);
 	cmd = strtok(NULL, "\n ");
 	char *name2 = strdup(cmd);
 
-	int id1 = get_user_id(name1);
+	int id1 = get_user_id(name1); //get the id of each name
 	int id2 = get_user_id(name2);
 
+	/* array in which is safed the distance from name1
+	to every other name
+	-> the vector is initialized in dfs with INF = 999
+	on every position
+	*/
 	int *distance = malloc(sizeof(int) * mg->nodes);
 	dfs(mg, id1, distance);
 
+	//if there is no link between the 2 names the value is INF
 	if (distance[id2] != INF)
 		printf("The distance between %s - %s is %d\n",
 			   name1, name2, distance[id2]);
 	else
 		printf("There is no way to get from %s to %s\n", name1, name2);
 
-	free(distance);
+	free(distance); //clear the memory
 	free(name1);
 	free(name2);
 }
 
 void common(char *cmd, matrix_graph_t *mg)
 {
-	cmd = strtok(NULL, "\n ");
+	cmd = strtok(NULL, "\n "); //get the names
 	char *name1 = strdup(cmd);
 	cmd = strtok(NULL, "\n ");
 	char *name2 = strdup(cmd);
 
-	int id1 = get_user_id(name1);
+	int id1 = get_user_id(name1); //get the id of each name
 	int id2 = get_user_id(name2);
 
-	int ok = 1;
+	int ok = 1; //verifies the 2 names have something in common
 	for (int i = 0; i < mg->nodes; i++)
+		//same column but different lines, link with the same thing
 		if (mg->matrix[id1][i] == 1 && mg->matrix[id2][i] == 1) {
 			ok = 0;
 			break;
@@ -549,17 +330,17 @@ void common(char *cmd, matrix_graph_t *mg)
 		printf("No common friends for %s and %s\n", name1, name2);
 	}
 
-	free(name1);
+	free(name1); //clear memory
 	free(name2);
 }
 
 int friends(char *name1, matrix_graph_t *mg)
 {
 	int id1 = get_user_id(name1);
-	int nr_of_friends = 0;
+	int nr_of_friends = 0; //returned value
 
 	for (int i = 0; i < mg->nodes; i++)
-		if (mg_has_edge(mg, id1, i))
+		if (mg_has_edge(mg, id1, i)) //one link = one friend
 			nr_of_friends++;
 
 	return nr_of_friends;
@@ -567,23 +348,26 @@ int friends(char *name1, matrix_graph_t *mg)
 
 void popular(char *cmd, matrix_graph_t *mg)
 {
-	cmd = strtok(NULL, "\n ");
+	cmd = strtok(NULL, "\n "); //get the name
 	char *name1 = strdup(cmd);
 
-	int id1 = get_user_id(name1);
+	int id1 = get_user_id(name1); //get the id of the name
+
+	//the array that stores the amount of friends of each name
 	int *most_popular = calloc(mg->nodes, sizeof(int));
 
+	//the array is completed
 	for (int i = 0; i < mg->nodes; i++)
 		if (mg_has_edge(mg, id1, i))
 			most_popular[i] = friends(get_user_name(i), mg);
 
-	int ok = 1;
+	int ok = 1; //verifies if the name is the most popular
 	int max_friends = friends(name1, mg);
 	int id = id1;
 	for (int i = 0; i < mg->nodes; i++)
 		if (most_popular[i] > max_friends) {
-			max_friends = most_popular[i];
-			id = i;
+			max_friends = most_popular[i]; //update the information
+			id = i;                        //of the most popular
 			ok = 0;
 		}
 
@@ -593,7 +377,7 @@ void popular(char *cmd, matrix_graph_t *mg)
 		printf("%s is the most popular friend of %s\n",
 			   get_user_name(id), name1);
 
-	free(most_popular);
+	free(most_popular); //clear memory
 	free(name1);
 }
 
